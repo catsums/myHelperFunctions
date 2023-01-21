6315347ece89496bf56e0b341301986ecc21116d
Cassim <111745304+catsums@users.noreply.github.com>
2023-01-21 18:21:07 +0200

    Using UMD for CDN

--- webpack.config.js
+++ webpack.config.js
@@ -3,42 +3,47 @@ const webpack = require('webpack');
 const devMode = process.env.NODE_ENV !== "production";
 
 module.exports = {
-  resolve: {
-    extensions: ['*', '.js'],
-    fallback: {
-      "fs": false,
-      "url": false,
-      "tls": false,
-      "net": false,
-      "path": false,
-      "zlib": false,
-      "http": false,
-      "https": false,
-      "stream": false,
-      "crypto": false,
-      "assert": false,
-      "os": false,
-    },
-  },
-//   entry: ["./lib/esm/index.mjs"],
-  entry: {
+	resolve: {
+		extensions: ['*', '.js'],
+		fallback: {
+			"fs": false,
+			"url": false,
+			"tls": false,
+			"net": false,
+			"path": false,
+			"zlib": false,
+			"http": false,
+			"https": false,
+			"stream": false,
+			"crypto": false,
+			"assert": false,
+			"os": false,
+		},
+	},
+//	 entry: ["./lib/esm/index.mjs"],
+	entry: {
 	"esm/index": "./lib/esm/index.mjs",
 	"cjs/index": "./lib/cjs/index.js",
-  },
-  output:{
-    path: path.resolve(__dirname, './lib/min'),
-    publicPath: '/',
-    filename: "[name].min.js"
-  },
-  mode: "production",
-  module:{
-    rules:[
-      {
-        test:/\.(js|jsx|mjs)/, 
-        exclude: /node_modules/, 
-        use:{ loader:"babel-loader" } 
-      },
-    ],
-  },
-  plugins: [],
+	},
+	output:{
+		path: path.resolve(__dirname, './lib/min'),
+		publicPath: '/',
+		filename: "[name].min.js",
+		globalObject: 'this',
+		library: {
+			name: "MY",
+			type: "umd"
+		},
+	},	
+	mode: "production",
+	module:{
+		rules:[
+			{
+				test:/\.(js|jsx|mjs)/, 
+				exclude: /node_modules/, 
+				use:{ loader:"babel-loader" } 
+			},
+		],
+	},
+	plugins: [],
 };
