a939cee1a5b2e7df334f1f569e12550574d11246
Cassim <111745304+catsums@users.noreply.github.com>
2023-11-15 21:31:48 +0200

    Added src files in ts

--- src/MyHelperFunctions.ts
+++ src/MyHelperFunctions.ts
@@ -1,4 +1,36 @@
-/*myHelperModule.ts*/
+
+declare global {
+	interface Array<T> {
+	  midCeil(): T;
+	  midFloor(): T;
+	  midRound(): T;
+	  last(): T;
+	  first(): T;
+	  removeAt(index : number): T;
+	  removeItem(item : T): T;
+	}
+
+	interface BigInt {
+		toJSON() : string;
+	}
+
+	interface Number {
+		mod(n : number) : number;
+	}
+
+	
+	interface IObject extends Object{
+		[key:string] : any
+	}
+
+	type JSONType = string|number|boolean|JSONType[]|IJSON|Object|null|undefined
+
+	interface IJSON extends Object{
+		[key:string] : JSONType
+	}
+
+}
+
 ///FOR WEB API FRONT-END MODULES
 
 
